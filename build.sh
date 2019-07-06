9a7cb270f51660f2d104b912efcef3acb0e655e1
evscott <eliotvscott@gmail.com>
2019-07-06 09:04:29 -0700

    initial commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+### shell script to build greenmap-db-worker
+go build -i -o $GOPATH/bin/greenmap/main cmd/main.go
---
+++ test/main_test.go
@@ -0,0 +1,7 @@
+package test
+
+import (
+	"testing"
+)
+
+func TestSanityCheck(t *testing.T) {}
